fc89417483599f9b0159b31909b1c59ddf976156
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2021-10-11 22:53:43 +0200

    upDate

--- trialevents/src/App.js
+++ trialevents/src/App.js
@@ -1,11 +1,33 @@
-
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <>
-    </>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      message: "Ready"
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      message: this.state.message === "Ready" ? "Clicked" : "Ready"
+    })
+  }
+  handleEvent = () => {
+    console.log("Wywołano metodę handleEvent");
+  }
+  render(){
+    return (
+      <>
+      <div className="m-2">
+        <div className="h4 bg-primary text-white text-center p-2">
+          {this.state.message}
+        </div>
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={this.handleEvent}>Click on me!</button>
+        </div>
+      </div>
+      </>
+    )
+  }
 }
-
-export default App;
